4fc0625c11fd9db075346afb8c4a3e6b9fff0d14
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-12-03 10:44:21 +0300

    update
    update

--- uninstall.php
+++ uninstall.php
@@ -15,11 +15,3 @@ $sides = get_posts( array( 'post_type' => 'side', 'numberposts' => -1 ) );
 foreach( $sides as $side ) {
 	wp_delete_post($side->ID, true );
 }
-
-///// ALTERNATIVE METHOD //////
-
-// Access the database via SQL
-//global $wpdb;
-//$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'side'" );
-//$wpdb->query( "DELETE FROM wp_postmeta WHERE post_id NOT IN ( SELECT id FROM wp_posts)" );
-//$wpdb->query( "DELETE FROM wp_term_relationships WHERE object_id NOT IN ( SELECT id FROM wp_posts)" );
